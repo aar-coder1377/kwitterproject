46bc230b39607c8e7c3a0955d3fd15017e431283
aar-coder1377 <82127736+aar-coder1377@users.noreply.github.com>
2021-09-08 16:11:27 -0700

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,30 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAN-tgAJ2W1_Kz4LD1p21BAjXeW8ylXlW0",
+    authDomain: "kwitterapp-29564.firebaseapp.com",
+    projectId: "kwitterapp-29564",
+    storageBucket: "kwitterapp-29564.appspot.com",
+    messagingSenderId: "241328383398",
+    appId: "1:241328383398:web:02e9bd1a012e773500db67",
+    measurementId: "G-9EF8QESB6Q"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function getData() {
+    firebase.database().ref("/" + room_name).on('value', function (snapshot) {
+        document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
+            childKey = childSnapshot.key; childData = childSnapshot.val(); if (childKey != "purpose") {
+                firebase_message_id = childKey;
+                message_data = childData;
+                //Start code
+
+
+
+
+                //End code
+            }
+        });
+    });
+}
+getData();
